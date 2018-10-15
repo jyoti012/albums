25f60e3d817feca0f277cfdb5e87472713b38bfc
Jyoti Suvarna <jyoti@ajackus.com>
2018-10-15 17:48:03 +0530

    Added font style to header component

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Text } from 'react-native';
 
 const Header = () => {
-    return <Text>Albums!</Text>;
+    const { textStyle } = styles;
+    return <Text style={textStyle}>Albums</Text>;
+};
+
+const styles = {
+	textStyle: {
+		fontSize: 35
+	}
 };
 
 // Make component available to other parts of the app
