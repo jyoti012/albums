3e86d0607830e0071805e6e33a573f225566347b
Jyoti Suvarna <jyoti@ajackus.com>
2018-10-16 16:39:41 +0530

    Added album banner

--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -4,23 +4,31 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetails = ({ album }) => {
-	const { title, artist, thumbnail_image } = album;
-	const { headerContentStyles, thumbnailStyle } = styles;
+	const { title, artist, thumbnail_image, image } = album;
+	const { 
+		headerContentStyles, 
+		headerTextStyle,
+		thumbnailStyle, 
+		thumbnailContainerStyle,
+		imageStyle
+	} = styles;
 
   return (
 		<Card>
 			<CardSection>
-				<View>
+				<View style={thumbnailContainerStyle} >
 					<Image 
 						style={thumbnailStyle} 
 						source={{ uri: thumbnail_image }} 
 					/>
 				</View>
 				<View style={headerContentStyles}>
-					<Text>{title}</Text>
+					<Text style={headerTextStyle}>{title}</Text>
 					<Text>{artist}</Text>
 				</View>
-				
+			</CardSection>
+			<CardSection>
+				<Image style={imageStyle} source={{ uri: image }} />
 			</CardSection>
 		</Card>
 	);
@@ -31,9 +39,23 @@ const styles = {
 		flexDirection: 'column',
 		justifyContent: 'space-around'
 	},
+	headerTextStyle: {
+		fontSize: 18
+	},
 	thumbnailStyle: {
 		height: 50,
 		width: 50
+	},
+	thumbnailContainerStyle: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10
+	},
+	imageStyle: {
+		height: 300,
+		flex: 1,
+		width: null
 	}
 };
 
