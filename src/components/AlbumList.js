2d01a64f55f0fdf9429235d7904aff3e9eeb1b27
Jyoti Suvarna <jyoti@ajackus.com>
2018-10-16 11:50:36 +0530

    Class based components

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-return (
-	<View >
-		<Text>Album Listing</Text>	
-	</View>
-	);
-};
+class AlbumList extends Component {
+	render() {
+		return (
+			<View >
+				<Text>Album Listing</Text>	
+			</View>
+		);
+	}
+}
 
 export default AlbumList;
